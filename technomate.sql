1d2cd8bf2a811e62c7fc80603d42676e4d9a20cb
Suraj Naveen <suraj0nb@gmail.com>
2023-10-23 23:13:34 +0530

    Update technomate.sql

--- technomate.sql
+++ technomate.sql
@@ -56,34 +56,34 @@ CREATE TABLE Medical (
     PRIMARY KEY(Medical_Id)
 )
 
---Adding foreign keys
-ALTER TABLE Department ADD FOREIGN KEY(Dean_ID) REFERENCES Dean(Dean_ID);
-ALTER TABLE Course ADD FOREIGN KEY(Department_ID) REFERENCES Department(Department_ID);
-ALTER TABLE Attendance ADD FOREIGN KEY(Student_ID) REFERENCES Student(Student_ID);
-ALTER TABLE Attendance ADD FOREIGN KEY(Course_code) REFERENCES Course(Course_code);
-ALTER TABLE Attendance ADD FOREIGN KEY(Medical_Id) REFERENCES Medical(Medical_Id);
-
-
 -- Insert sample data into the tables
 -- Insert sample data into the Department table
 INSERT INTO Department (Department_ID, Name, Dean_ID)
 VALUES
-    ('D1001', 'Computer Science', 'F2001'),
-    ('D1002', 'Electrical Engineering', 'F2002'),
-    ('D1003', 'Mechanical Engineering', 'F2003'),
-    ('D1004', 'Mathematics', 'F2004');
+    ('DP001', 'Information and Communication Technology', 'F2001'),
+    ('DP002', 'Engineering Technology', 'F2002'),
+    ('DP003', 'Bio Systems Technology', 'F2003'),
+    ('DP004', 'Multidisiplinary Studies', 'F2004');
 
 -- Insert sample data into the Course table
 INSERT INTO Course (Course_code, Name, Credit, Course_hours, Course_type, Department_ID)
 VALUES
-    ('CSE101', 'Introduction to Programming', 3, 45, 'GPA', 'D1001'),
-    ('EEE201', 'Circuit Analysis', 4, 60, 'Non GPA', 'D1002'),
-    ('MECH301', 'Thermodynamics', 3, 45, 'GPA', 'D1003'),
-    ('MATH101', 'Calculus I', 3, 45, 'Non GPA', 'D1004'),
-    ('CSE102', 'Data Structures', 3, 45, 'GPA', 'D1001'),
-    ('EEE202', 'Digital Electronics', 4, 60, 'Non GPA', 'D1002'),
-    ('MECH302', 'Fluid Mechanics', 3, 45, 'GPA', 'D1003'),
-    ('MATH102', 'Calculus II', 3, 45, 'Non GPA', 'D1004'),
+    ('ICT1122', 'Introduction to Programming', 3, 45, 'GPA', 'DP001'),
+    ('ICT1213', 'Descrete Mathamatics', 4, 60, 'GPA', 'DP001'),
+    ('ICT1253', 'Computer Networkig', 3, 45, 'GPA', 'DP001'),
+    ('ICT1212', 'DBMS', 3, 45, 'GPA', 'DP001'),
+    ('ICT1242', 'Computer Architecher', 3, 45, 'GPA', 'DP001'),
+    ('TMS2022', 'Physics', 4, 60, 'GPA', 'DP003'),
+    ('TMS4302', 'Calculus', 3, 45, 'GPA', 'DP003'),
+    ('TMS6301', 'Wrokshop', 3, 45, 'GPA', 'DP003'),
+    ('TMS6302', 'Electricity and Magnatisam', 3, 45, 'GPA', 'DP003'),
+    ('TMS7302', 'Properties Of Matters', 3, 45, 'GPA', 'DP003'),
+    ('BST4024', 'Fisheries', 3, 45, 'GPA', 'DP003'),
+    ('BST3021', 'Micro Biology', 3, 45, 'GPA', 'DP003'),
+    ('BST2343', 'Genetics', 3, 45, 'GPA', 'DP003'),
+    ('BST2331', 'Electronic', 3, 45, 'GPA', 'DP003'),
+    ('ENG1212', 'English', 3, 45, 'Non GPA', 'DP004'),
+    ('TCS1212', 'Management', 3, 45, 'Non GPA', 'DP004');
 
 -- Insert sample data into the Marks table
 INSERT INTO Marks (Marks_ID, Mid_marks, Final_marks, GPA)
@@ -112,14 +112,53 @@ VALUES
 -- Insert sample data into the Attendance table
 INSERT INTO Attendance (Attendance_ID, Date, Status, Course_code, Student_ID, Medical_Id)
 VALUES
-    ('ATT3001', '2023-03-10', 'Present', 'CSE101', 'S001', NULL),
-    ('ATT3002', '2023-03-12', 'Absent', 'EEE201', 'S002', 'M1001'),
-    ('ATT3003', '2023-03-14', 'Present', 'MECH301', 'S003', NULL),
-    ('ATT3004', '2023-03-10', 'Absent', 'CSE101', 'S004', 'M1002'),
-    ('ATT3005', '2023-03-12', 'Present', 'EEE201', 'S005', NULL),
-    ('ATT3006', '2023-03-14', 'Present', 'MECH301', 'S006', NULL),
-    ('ATT3014', '2023-03-12', 'Present', 'EEE201', 'S014', NULL),
-    ('ATT3015', '2023-03-14', 'Absent', 'MECH301', 'S015', NULL);
+    ('ATTIT002', '2023-03-10', 'Present', 'ICT1122', 'S4', NULL),
+    ('ATTIT003', '2023-03-10', 'Present', 'ICT1122', 'S5', NULL),
+    ('ATTIT004', '2023-03-10', 'Present', 'ICT1122', 'S8', NULL),
+    ('ATTIT005', '2023-03-10', 'Absant', 'ICT1122', 'S11', NULL),
+    ('ATTIT006', '2023-03-10', 'Present', 'ICT1122', 'S13', NULL),
+
+    ('ATTIT008', '2023-03-10', 'Present', 'ICT1212', 'S4', NULL),
+    ('ATTIT009', '2023-03-10', 'Present', 'ICT1212', 'S5', NULL),
+    ('ATTIT010', '2023-03-10', 'Present', 'ICT1212', 'S8', NULL),
+    ('ATTIT011', '2023-03-10', 'Absant', 'ICT1212', 'S11', NULL),
+    ('ATTIT012', '2023-03-10', 'Present', 'ICT1212', 'S13', NULL),
+
+    ('ATTBT001', '2023-03-10', 'Present', 'BST4024', 'S7', NULL),
+    ('ATTBT002', '2023-03-10', 'Present', 'BST4024', 'S10', NULL),
+    ('ATTBT003', '2023-03-10', 'Absent', 'BST4024', 's15', 'M1001'),
+    ('ATTBT004', '2023-03-10', 'Present', 'BST2343', 'S7', NULL),
+    ('ATTBT005', '2023-03-10', 'Present', 'BST2343', 'S10', NULL),
+    ('ATTBT006', '2023-03-10', 'Absent', 'BST2343', 's15', 'M1001'),
+
+    ('ATTET003', '2023-03-10', 'Present', 'TMS2022', 'S6', NULL),
+    ('ATTET004', '2023-03-10', 'Absent', 'TMS2022', 'S9', 'M1002'),
+    ('ATTET005', '2023-03-10', 'Present', 'TMS2022', 'S12', NULL),
+    ('ATTET006', '2023-03-10', 'Present', 'TMS2022', 'S14', NULL),
+
+    ('ATTET008', '2023-03-11', 'Present', 'TMS6302', 'S6', NULL),
+    ('ATTET009', '2023-03-11', 'Absent', 'TMS6302', 'S9', 'M1002'),
+    ('ATTET010', '2023-03-11', 'Present', 'TMS6302', 'S12', NULL),
+    ('ATTET011', '2023-03-11', 'Present', 'TMS6302', 'S14', NULL),
+
+    ('ATTIT013', '2023-03-11', 'Present', 'ICT1253', 'S4', NULL),
+    ('ATTIT014', '2023-03-11', 'Present', 'ICT1253', 'S5', NULL),
+    ('ATTIT015', '2023-03-11', 'Present', 'ICT1253', 'S8', NULL),
+    ('ATTIT016', '2023-03-11', 'Absant', 'ICT1253', 'S11', NULL),
+    ('ATTIT017', '2023-03-11', 'Present', 'ICT1253', 'S13', NULL),
+
+    ('ATTBT007', '2023-03-11', 'Present', 'BST4024', 'S7', NULL),
+    ('ATTBT008', '2023-03-11', 'Present', 'BST4024', 'S10', NULL),
+    ('ATTBT009', '2023-03-11', 'Absent', 'BST4024', 's15', 'M1001'),
+    ('ATTBT0010', '2023-03-11', 'Present', 'BST2343', 'S7', NULL),
+    ('ATTBT0011', '2023-03-11', 'Present', 'BST2343', 'S10', NULL),
+    ('ATTBT0012', '2023-03-11', 'Absent', 'BST2343', 's15', 'M1001'),
+
+    ('ATTET007', '2023-03-11', 'Present', 'MECH301', 'S3', NULL),
+    ('ATTET008', '2023-03-11', 'Absent', 'CSE101', 'S4', 'M1002'),
+    ('ATTET009', '2023-03-11', 'Present', 'EEE201', 'S5', NULL),
+    ('ATTET010', '2023-03-11', 'Present', 'MECH301', 'S6', NULL),
+    ('ATTET011', '2023-03-11', 'Absent', 'MECH301', 'S7', NULL);
 
 
 -- Insert sample data into the Notice table
@@ -136,5 +175,12 @@ VALUES
 -- Insert sample data into the Medical table
 INSERT INTO Medical (Medical_Id, Start_Date, End_Date)
 VALUES
-    ('M1001', '2023-03-12', '2023-03-14'),
-    ('M1002', '2023-04-10', '2023-04-11');
+    ('M1001', '2023-03-10', '2023-03-12'),
+    ('M1002', '2023-03-10', '2023-03-11');
+
+--Adding foreign keys
+ALTER TABLE Department ADD FOREIGN KEY(Dean_ID) REFERENCES Dean(Dean_ID);
+ALTER TABLE Course ADD FOREIGN KEY(Department_ID) REFERENCES Department(Department_ID);
+ALTER TABLE Attendance ADD FOREIGN KEY(Student_ID) REFERENCES Student(Student_ID);
+ALTER TABLE Attendance ADD FOREIGN KEY(Course_code) REFERENCES Course(Course_code);
+ALTER TABLE Attendance ADD FOREIGN KEY(Medical_Id) REFERENCES Medical(Medical_Id);
